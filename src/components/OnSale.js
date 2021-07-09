ca1fc840b4e6a34ae041f1146fd5e1f584e5e266
Federico L. Martin <fl.martin.dev@gmail.com>
2021-07-09 15:27:04 -0300

    Adding date onSale

--- src/components/OnSale.js
+++ src/components/OnSale.js
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import styles from "../css/ItemsDisplay.module.css";
+import { format, add, differenceInHours, hoursToMinutes } from "date-fns";
 
 function OnSale() {
 	const [displayData, setDisplayData] = useState([]);
 
-	const untilDate = new Date().toString();
+	const date = new Date();
+	const futureDate = add(date, {
+		days: 1,
+	});
+	const remains = differenceInHours(futureDate, date);
+	const remainsMinutes = hoursToMinutes(remains);
+
+	const untilDate = format(futureDate, "dd/MM/yyyy");
 
 	useEffect(
 		() =>
@@ -18,33 +26,38 @@ function OnSale() {
 	);
 
 	return (
-		<div className={styles.ItemsDisplay}>
-			<h1 className={styles.dateOnSale}>
-				Products on sale until {untilDate}!🔥
-			</h1>
-			{displayData.map((item) => {
-				if (item.price < 50) {
-					return (
-						<div key={item.id} className={styles.Item}>
-							<img
-								alt={item.title}
-								src={item.image}
-								className={styles.ItemPhoto}
-							/>
-							<h2>${item.price}</h2>
-							<h3 className={styles.ItemTitle}>{item.title}</h3>
-							<div className={styles.itemButtons}>
-								<button className={styles.detailButton}>
-									Details
-								</button>
-								<button className={styles.buyButton}>
-									Add to cart
-								</button>
+		<div>
+			<div className={styles.dateOnSale}>
+				Products on sale until <br />
+				{untilDate}!🔥
+			</div>
+			<div className={styles.ItemsDisplay}>
+				{displayData.map((item) => {
+					if (item.price < 50) {
+						return (
+							<div key={item.id} className={styles.Item}>
+								<img
+									alt={item.title}
+									src={item.image}
+									className={styles.ItemPhoto}
+								/>
+								<h2 className={styles.price}>${item.price}</h2>
+								<h3 className={styles.ItemTitle}>
+									{item.title}
+								</h3>
+								<div className={styles.itemButtons}>
+									<button className={styles.detailButton}>
+										Details
+									</button>
+									<button className={styles.buyButton}>
+										Add to cart
+									</button>
+								</div>
 							</div>
-						</div>
-					);
-				} else return null;
-			})}
+						);
+					} else return null;
+				})}
+			</div>
 		</div>
 	);
 }
